da5a08fdad16121d145ed2e587fa68c77102d0ef
devilyugg <2216266046@qq.com>
2019-11-06 12:00:45 +0800

    修改

---
+++ mybg/jquery.bg.js
@@ -0,0 +1,19 @@
+
+
+(function(){
+    
+    $.fn.bg=function(bgcolor){
+        //this代表当前条用方法的对象
+        this.css('background',bgcolor)
+        return this
+    },
+
+    $.fn.fontC=function(fontcolor){
+        //this代表当前条用方法的对象
+        this.css('color',fontcolor)
+        return this
+    }
+
+    
+
+})()
